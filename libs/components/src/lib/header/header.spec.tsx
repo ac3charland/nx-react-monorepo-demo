7effc7dea49580cf2a5d840f858c7f856cc763f8
Alex Charland <ac3charland@gmail.com>
2024-06-26 15:16:23 -0500

    Add Jest snapshot example test

--- libs/components/src/lib/header/header.spec.tsx
+++ libs/components/src/lib/header/header.spec.tsx
@@ -1,9 +1,14 @@
-import { render } from '@testing-library/react';
 import { Header } from './header';
+import { render } from '@testing-library/react';
 
 describe('Components', () => {
   it('should render successfully', () => {
     const { baseElement } = render(<Header />);
     expect(baseElement).toBeTruthy();
+  });
+
+  it('should match snapshot', () => {
+    const { baseElement } = render(<Header />);
+    expect(baseElement).toMatchSnapshot();
   });
 });
